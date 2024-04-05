31191b434f1e640001a8074a9b12847d8159e6b0
Karson Nguyen <karsonn@sfu.ca>
2024-04-05 00:35:07 -0700

    Initial commit

---
+++ SkinnyAnimals/Configuration.cs
@@ -0,0 +1,7 @@
+namespace SkinnyAnimals; 
+
+internal class Configuration {
+    public bool Enabled { get; set; } = true;
+    public int PushSpeedMultiplier { get; set; } = 2;
+    public bool IgnoreCollision { get; set; }
+}
---
+++ SkinnyAnimals/Mod.cs
@@ -0,0 +1,63 @@
+﻿using HarmonyLib;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+
+namespace SkinnyAnimals;
+
+internal partial class Mod: StardewModdingAPI.Mod {
+    public static Configuration Config;
+
+    public override void Entry(IModHelper helper) {
+        Config = Helper.ReadConfig<Configuration>();
+
+        helper.Events.GameLoop.GameLaunched += OnGameLaunched;
+        ApplyHarmonyPatches();
+    }
+    
+    private void ApplyHarmonyPatches() {
+        var harmony = new Harmony(ModManifest.UniqueID);
+
+        harmony.Patch(
+            original: AccessTools.Method(typeof(FarmAnimal), nameof(FarmAnimal.farmerPushing)),
+            postfix: new HarmonyMethod(typeof(FarmAnimal_farmerPushing_Patch),
+                nameof(FarmAnimal_farmerPushing_Patch.Postfix))
+        );
+    }
+
+    private void OnGameLaunched(object sender, GameLaunchedEventArgs e) {
+        var configMenu = Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+        if (configMenu is not null) RegisterConfig(configMenu);
+    }
+
+    private void RegisterConfig(IGenericModConfigMenuApi configMenu) {
+        configMenu.Register(
+            mod: ModManifest,
+            reset: () => Config = new Configuration(),
+            save: () => Helper.WriteConfig(Config)
+        );
+        
+        configMenu.AddBoolOption(
+            mod: ModManifest,
+            name: () => "Enabled", 
+            getValue: () => Config.Enabled,
+            setValue: value => Config.Enabled = value
+        );
+        
+        configMenu.AddNumberOption(
+            mod: ModManifest,
+            name: () => "Push Speed Multiplier", 
+            getValue: () => Config.PushSpeedMultiplier,
+            setValue: value => Config.PushSpeedMultiplier = value,
+            min: 1,
+            max: 10
+        );
+        
+        configMenu.AddBoolOption(
+            mod: ModManifest,
+            name: () => "Ignore Collision", 
+            getValue: () => Config.IgnoreCollision,
+            setValue: value => Config.IgnoreCollision = value
+        );
+    }
+}
